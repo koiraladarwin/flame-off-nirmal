f7f54c071c361b68218fe2ffc3806015e3807247
Darwin Koirala <darwinkoirala123@gmail.com>
2025-06-25 14:49:36 +0545

    made some ui changes

--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -61,15 +61,15 @@ function Notice() {
   return (
     <div
       ref={containerRef}
-      className="h-auto bg-white flex flex-col p-4 gap-4 md:flex-row md:pl-10 md:py-20 relative rounded-3xl md:gap-10"
+      className="h-auto flex flex-col p-4 gap-4 md:flex-row md:pl-10 md:py-20 relative rounded-3xl md:gap-10 md:mt-28 md:mb-28"
     >
       {/* top part */}
-      <div ref={titleRef} className="absolute top-5 left-5">
-        <h1 className="text-4xl font-bold">News and Events</h1>
+      <div ref={titleRef} className="absolute top-5 left-5 md:left-20">
+        <h1 className="text-6xl font-bold">News and Events</h1>
       </div>
 
       {/* left part */}
-      <div ref={leftRef} className="flex flex-col mt-20 md:mt-0">
+      <div ref={leftRef} className="flex flex-col mt-20 md:ml-20 md:mt-0">
         <h2 className="text-4xl font-black mt-10">
           Latest <br /> News
         </h2>
@@ -80,7 +80,7 @@ function Notice() {
       </div>
 
       {/* right part */}
-      <div className="flex justify-center items-center flex-col flex-1 mr-4 md:flex-1 mt-10 md:mt-0">
+      <div className="flex justify-center items-center flex-col flex-1 mr-4 md:flex-1 mt-10 md:mt-10 md:mr-40">
         <div
           ref={(el) => (cardsRef.current[0] = el)}
           className="bg-blue-100 w-full p-5 rounded-xl mb-4"
@@ -96,6 +96,23 @@ function Notice() {
             nemo eveniet, quasi ea at facere quam unde ullam...
           </p>
         </div>
+
+        <div
+          ref={(el) => (cardsRef.current[0] = el)}
+          className="bg-blue-100 w-full p-5 rounded-xl mb-4"
+        >
+          <p className="mb-2">
+            <span className="text-xl font-black text-orange-500 uppercase">
+              Category
+            </span>
+            <span className="text-xl font-black ml-2">JUNE 2025</span>
+          </p>
+          <p className="tracking-wider">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
+            nemo eveniet, quasi ea at facere quam unde ullam...
+          </p>
+        </div>
+
 
         <div
           ref={(el) => (cardsRef.current[1] = el)}

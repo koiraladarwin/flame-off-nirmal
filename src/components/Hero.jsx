0e67a143e89d896fb768fbef0b27e081a9d80838
Darwin Koirala <darwinkoirala123@gmail.com>
2025-06-25 10:22:02 +0545

    typo changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -145,7 +145,7 @@ const Hero = () => {
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
-              falme o<b>f</b>f
+              flame o<b>f</b>f
             </h1>
 
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
